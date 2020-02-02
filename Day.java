648329d75f9eee28dabeec6f67e4dbe94b4a389a
Thomas Urech <48928679+turech@users.noreply.github.com>
2020-02-01 19:28:27 -0600

    Update Day
    now takes dd/mm/yyyy format

--- Day.java
+++ Day.java
@@ -1,8 +1,18 @@
 package planner;
 
 public class Day {
-	public Day(int which)
+	private String date;
+	public Day(int day, int month, int year)
 	{
-		
+		String d = String.format("%2d", day);
+		String m = String.format("%2d", month);
+		String y = String.format("%4d", year);
+		date = d + "/" + m + "/" + y;
+	}
+	
+	public String toString()
+	{
+		return date;
 	}
 }
+
