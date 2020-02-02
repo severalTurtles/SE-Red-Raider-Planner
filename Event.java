52db325604405d210ce67de3f0e4212e05fddd8a
Omer571 <53447305+Omer571@users.noreply.github.com>
2020-02-01 19:34:38 -0600

    Adding empty event class with constructors.

---
+++ Event.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package planner;
+
+import java.util.*;
+
+/**
+ *
+ * @author macbookretina2015
+ */
+public class Event {
+    Event(String date, int start, int end, String eventName, String description) {
+        
+    }
+    
+    Event(Date date, int start, int end, String eventName) {
+        
+    }
+}
