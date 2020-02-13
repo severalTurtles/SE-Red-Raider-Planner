7912955cd202caa6e993da60a7dfa546da986058
Thomas Urech <48928679+turech@users.noreply.github.com>
2020-02-12 21:41:05 -0600

    Update RRPrunner - Thomas
    the actual functioning main method. 4 edge-case days with 6 events pre-prepared

--- RRPrunner.java
+++ RRPrunner.java
@@ -3,6 +3,8 @@
  */
 package planner;
 
+import java.util.Scanner;
+
 /**
  * @author tfure
  *
@@ -10,13 +12,20 @@ package planner;
 public class RRPrunner {
 	public static void main(String args[])
 	{
-		System.out.print("Cale Enright\n"
-				+ "Brianna Rodriguez\n"
-				+ "Omer Quershi\n"
-				+ "Stephen Paternoster\n"
-				+ "Thomas Urech\n"
-				);
-		
+		Scanner scan = new Scanner(System.in);
+		Calendar cal = new Calendar(2020);
+		Day t1 = cal.getDay(1, 1);
+		Day t2 = cal.getDay(1, 31);
+		Day t3 = cal.getDay(2, 1);
+		Day t4 = cal.getDay(2, 29);
+		t1.addEvent(0, 30, "event1", "Happy new year");
+		t1.addEvent(100, 1200, "event2", "I'm going back to bed");
+		t1.addEvent(1200, 1800, "event3", "Oh no gotta deal with guests");
+		t2.addEvent(0, 2359, "event4", "Happy Birthday James");
+		t3.addEvent(1600, 2000, "event5", "I hope Mahomes wins the Super Bowl");
+		t4.addEvent(800, 2000, "event6", "Wait it isn't March?");
+		RPPlanerIO runner = new RPPlanerIO(scan,cal);
+		runner.run();
 	}
 }
 
